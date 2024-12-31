90c1be6cda01133b1638bd1a2219212bcc138c72
kk9uk <122882463+kk9uk@users.noreply.github.com>
2025-01-01 06:34:03 +0800

    [FEAT]: Group Anagrams

---
+++ LeetCode/GroupAnagrams.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> res = new HashMap<>();
+        for (String str : strs) {
+            int[] curr = new int[26];
+            for (int i = 0; i < str.length(); i++)
+                curr[str.charAt(i) - 'a']++;
+            res.merge(
+                    Arrays.toString(curr),
+                    new ArrayList<>(List.of(str)),
+                    (old, _) -> {
+                        old.add(str);
+                        return old;
+                    }
+            );
+        }
+        return new ArrayList<>(res.values());
+    }
+}
