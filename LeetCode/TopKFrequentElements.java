613a3e94640c20c97d4050729ac4579cb9af52f9
kk9uk <122882463+kk9uk@users.noreply.github.com>
2025-01-01 08:56:26 +0800

    [FEAT]: Top K Frequent Elements

---
+++ LeetCode/TopKFrequentElements.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class TopKFrequentElements {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for (int num : nums)
+            count.merge(num, 1, (old, val) -> old + val);
+
+        // counting sort by freq
+        List<Integer>[] tnuoc = new List[nums.length + 1];
+        for (var entry : count.entrySet()) {
+            if (tnuoc[entry.getValue()] == null)
+                tnuoc[entry.getValue()] = new ArrayList<>();
+            tnuoc[entry.getValue()].add(entry.getKey());
+        }
+
+        int[] res = new int[k];
+        for (int i = tnuoc.length - 1, resI = 0; i >= 0 && resI < k; i--)
+            if (tnuoc[i] != null)
+                for (int j = 0; j < tnuoc[i].size(); j++)
+                    res[resI++] = tnuoc[i].get(j);
+        return res;
+    }
+}
