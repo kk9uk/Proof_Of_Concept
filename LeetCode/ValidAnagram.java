84feb16d7f425de13c838838cba7863eadf681ab
kk9uk <122882463+kk9uk@users.noreply.github.com>
2025-01-01 08:45:00 +0800

    [REFACTOR]: HashMap's merge() is lovely<3

--- LeetCode/ValidAnagram.java
+++ LeetCode/ValidAnagram.java
@@ -6,11 +6,11 @@ public class ValidAnagram {
     public boolean isAnagram(String s, String t) {
         HashMap<Character, Integer> countS = new HashMap<>();
         for (int i = 0; i < s.length(); i++)
-            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+            countS.merge(s.charAt(i), 1, (a, b) -> a + b);
 
         HashMap<Character, Integer> countT = new HashMap<>();
         for (int i = 0; i < t.length(); i++)
-            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+            countT.merge(t.charAt(i), 1, (a, b) -> a + b);
 
         return countS.equals(countT);
     }
