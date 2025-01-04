6fca7a00ce733d3dffd4319d175ff7d27e93f626
kk9uk <122882463+kk9uk@users.noreply.github.com>
2025-01-04 09:53:24 +0800

    [FEAT]: Product of Array Except Self

---
+++ LeetCode/ProductOfArrayExceptSelf.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+
+        // init with prefix product
+        for (int i = 0, accum = 1; i < res.length; i++) {
+            res[i] = accum;
+            accum *= nums[i];
+        }
+
+        // times suffix product
+        for (int i = res.length - 1, accum = 1; i >= 0; i--) {
+            res[i] *= accum;
+            accum *= nums[i];
+        }
+
+        return res;
+    }
+}
